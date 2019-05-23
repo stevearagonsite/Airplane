d616bb0ee115598b22396d52610f0489ba31a3d5
steve aragon <stevearagon92@gmail.com>
2019-05-23 00:39:17 -0300

    Create and integrate APIs Photon and TextMesh

---
+++ Assets/Scripts/UI/UserConnectionStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using TMPro;
+
+public class UserConnectionStatus : MonoBehaviour
+{
+    private readonly string connectionStatusMessage = "Connection Status:";
+
+    [Header("UI References")]
+    public TextMeshProUGUI ConnectionStatusText;
+
+
+    public void Update()
+    {
+        ConnectionStatusText.SetText($"{connectionStatusMessage} {PhotonNetwork.NetworkClientState}");
+    }
+}
---
+++ Assets/Scripts/UI/UserLobbyPanels.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Realtime;
+using Photon.Pun;
+using TMPro;
+using System;
+
+public class UserLobbyPanels : MonoBehaviourPunCallbacks
+{
+    [Header("Login")]
+    public GameObject LoginPanel;
+    public TMP_InputField PlayerNameInput;
+    public Dictionary<LobbyPanel, Action> onActivePanels = new Dictionary<LobbyPanel, Action>();
+
+    private void Start()
+    {
+        onActivePanels.Add(LobbyPanel.Login,      delegate { });
+        onActivePanels[LobbyPanel.ListRoom] += () => { };
+
+        onActivePanels.Add(LobbyPanel.CreateRoom, delegate { });
+        onActivePanels[LobbyPanel.ListRoom] += () => { };
+    
+        onActivePanels.Add(LobbyPanel.RandomRoom, delegate { });
+        onActivePanels[LobbyPanel.ListRoom] += () => { };
+
+        onActivePanels.Add(LobbyPanel.ListRoom,   delegate { });
+        onActivePanels[LobbyPanel.ListRoom] += () => { };
+    }
+
+    public void OnLoginButtonClicked()
+    {
+        string userID = PlayerNameInput.text;
+
+        if (!string.IsNullOrEmpty(userID))
+        {
+            PhotonNetwork.LocalPlayer.NickName = userID;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.LogError("Player Name is invalid.");
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+    }
+
+    private void SetActivePanel(LobbyPanel activePanel)
+    {
+        onActivePanels[activePanel]();
+    }
+}
+
+public enum LobbyPanel
+{
+    Login,
+    CreateRoom,
+    RandomRoom,
+    ListRoom
+}
